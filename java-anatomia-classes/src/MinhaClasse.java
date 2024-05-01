e5ef5a3e8418acf11e19e908ed3beb6dc41f8620
Leo <leonardoperin70@gmail.com>
2024-05-01 17:52:48 -0300

    docs: update MinhaClasse.java

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,25 @@
-public class MinhaClasse {  //Aqui estamos iniciando a minha classe com o mesmo nome do arquivo e a primeira letra maiuscula
+public class MinhaClasse {  //Aqui estamos iniciando a minha classe com o mesmo nome do arquivo e a primeira letra maiuscula, caso for composta a segunda tambem
     public static void main(String[] args) {  //Aqui temos o metodo principla chamado de main
         System.out.println("Olá a todos!!");    //Aqui estamos entregando um "valor" para o metodod print
+
+        int numero = 19;
+        numero = 20;
+
+        final String BR = "Brasil"; //Quando for uma variavel que nunca vai ser alterada deve se utilizar o "final" junto das letras maiuscula
+
+        // Declação inválida de variáveis
+
+            // String nome&sobrenome = "Leo Berso"; //Os únicos símbolos permitidos são _ e $
+            // int 20numero = 20;                   //Uma variável não pode começar com númerico
+            // String pao de mel = "Pao de mel";    //Não pode ter espaço no nome da variável
+            // int long = 1;                       //Long faz parte das palavras reservadas da linguagem
+ 
+        // Declaração válida de veriáveis
+            String nome$sobrenome = "Leo Berso";
+            int numero20 = 20;
+            String paoDeMel = "Pao de mel";
+            int longo = 1;
+        
+            
     }
 }
