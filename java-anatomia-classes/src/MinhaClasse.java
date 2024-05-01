03fcb2af6fc3e9f06fb772d0d1a806e29176db9b
Leo <leonardoperin70@gmail.com>
2024-05-01 17:27:00 -0300

    feat: add MinhaClasse

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,5 @@
+public class MinhaClasse {  //Aqui estamos iniciando a minha classe com o mesmo nome do arquivo e a primeira letra maiuscula
+    public static void main(String[] args) {  //Aqui temos o metodo principla chamado de main
+        System.out.println("Olá a todos!!");    //Aqui estamos entregando um "valor" para o metodod print
+    }
+}
