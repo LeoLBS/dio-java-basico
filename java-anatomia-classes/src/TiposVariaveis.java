d785a091e4fed31c41f5a76a8a57df80695d75f0
Leonardo Berso <leonardo.berso@atak.com.br>
2024-05-02 13:21:12 -0300

    feat: add TiposVariaveis.java

---
+++ java-anatomia-classes/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        
+        int numero = 2; //Aqui temos a variavel do tipo int, onde aceita números do tipo inteiro;
+
+        double numero2 = 2.5; //Aqui temos a variavel do tipo double, onde aceita números do tipo real(pontos flutuantes);
+
+        String nome = "Leo" //Aqui temos a variavel do tipo string, onde aceita letras("A","B","C"...)
+
+        boolean verdadeiroFalso = true; //Aqui temos a variavel do tipo boolean, onde aceita valores logicos(falso ou verdadeiro);
+
+    }
+}
